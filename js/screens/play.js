d2c535b543fb5be3907c55076e6b146d4f2110c3
seehafem <46550647+seehafem@users.noreply.github.com>
2020-04-20 21:23:11 -0500

    Changed to Level 1; needed to commit to be allowed to pull

--- js/screens/play.js
+++ js/screens/play.js
@@ -1,41 +1,39 @@
 game.PlayScreen = me.ScreenObject.extend({
-    /**
-     *  action to perform on state change
-     */
+	
     onResetEvent: function() {
-		
-		function addRandomEnemy() {
-			var choice = Math.floor(Math.random() * 3) + 1;
+        //Function to add one of the three different skeleton types at random
+        function addRandomEnemy() {
+            var choice = Math.floor(Math.random() * 3) + 1;
             if (choice == 1) {
-				me.game.world.addChild(me.pool.pull("clothedSkeleton", 0, 192));
-			}
+                me.game.world.addChild(me.pool.pull("clothedSkeleton", 0, 192));
+            }
             else if (choice == 2) {
-				me.game.world.addChild(me.pool.pull("robedSkeleton", 0, 192));
-			}
+                me.game.world.addChild(me.pool.pull("robedSkeleton", 0, 192));
+            }
             else {
-				me.game.world.addChild(me.pool.pull("armoredSkeleton", 0, 192));
-			}
-		}
-		
-		// Load first level * CHANGE LOADLEVEL TO TEST YOUR LEVEL *
-		me.levelDirector.loadLevel("level01");
+                me.game.world.addChild(me.pool.pull("armoredSkeleton", 0, 192));
+            }
+        }
+
+        // Load first level with a black background covering the default melonJS background
+        me.levelDirector.loadLevel("level01");
         me.game.world.addChild(new me.ColorLayer("background", "#000000"), 0);
 		
-		// Generate enemies * COMMENT OUT UNTIL YOUR LEVEL IS READY FOR ENEMIES (COLLISION OBJECTS SET UP) *
-		addRandomEnemy();
+		// Generate random enemies every five seconds
+        addRandomEnemy();
         me.timer.setInterval(addRandomEnemy, 5000, true);
 		
-        // reset the score
+        // FROM BOILERPLATE: Reset the score
         game.data.score = 0;
 
-        // Add our HUD to the game world, add it last so that this is on top of the rest.
+        // FROM BOILERPLATE: Add our HUD to the game world, add it last so that this is on top of the rest.
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
     },
 
     /**
-     *  action to perform when leaving this screen (state change)
+     *  ALL FROM BOILERPLATE: action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
         // remove the HUD from the game world
