9c1e39db94241d068b3c230fe0d867df6663584c
Nicole Shumer <shumern@oregonstate.edu>
2020-05-24 16:51:40 -0600

    Game balance changes

--- js/screens/play.js
+++ js/screens/play.js
@@ -4,85 +4,64 @@
  * CITATION: This code is adapted from the MelonJS Boilerplate
  *
  ********************************************************************/
+
+// 1 enemy combinations
+var c1C = ["clothedSkeleton"];
+var c1R = ["robedSkeleton"];
+var c1A = ["armoredSkeleton"];
+
+// 2 enemy combinations
+var c2CR = ["clothedSkeleton", "robedSkeleton"];
+var c2CCR = ["clothedSkeleton", "clothedSkeleton", "robedSkeleton"];
+var c2CRR = ["clothedSkeleton", "robedSkeleton", "robedSkeleton"];
+
+var c2CA = ["clothedSkeleton", "armoredSkeleton"];
+var c2CCA = ["clothedSkeleton", "clothedSkeleton", "armoredSkeleton"];
+var c2CAA = ["clothedSkeleton", "armoredSkeleton", "armoredSkeleton"];
+
+var c2RA = ["robedSkeleton", "armoredSkeleton"];
+var c2RRA = ["robedSkeleton", "robedSkeleton", "armoredSkeleton"];
+var c2RAA = ["robedSkeleton", "armoredSkeleton", "armoredSkeleton"];
+
+// 3 enemy combinations
+var c3CRA = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
+var c3CCRA = ["clothedSkeleton", "clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
+var c3CRRA = ["clothedSkeleton", "robedSkeleton", "robedSkeleton", "armoredSkeleton"];
+var c3CRAA = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton", "armoredSkeleton"];
+
 game.PlayScreen = me.Stage.extend({
 
     onResetEvent: function() {
-		
-		// Level enemy compositions:
-		var level = {};
+        // Level enemy compositions:
+        var level = {};
 
-		// Set composition of each wave for Level 01
-		if (game.data.level == 0) {
+        // Set composition of each wave for Level 01
+        if (game.data.level == 0) {
+            level.enemies = [c1C, c2CCR, c2CRR, c3CCRA, c2CCA, c2RRA, c3CRA, c2RA, c3CRA, c3CRAA];
+            level.counts = [2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+            level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 25];
+        }
 
-			var w1 = ["clothedSkeleton"];
-			var w2 = ["clothedSkeleton", "clothedSkeleton", "robedSkeleton"];
-			var w3 = ["clothedSkeleton", "robedSkeleton", "robedSkeleton"];
-			var w4 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var w5 = ["clothedSkeleton", "clothedSkeleton", "armoredSkeleton"];
-			var w6 = ["robedSkeleton", "armoredSkeleton"];
-			var w7 = ["robedSkeleton", "armoredSkeleton"];
-			var w8 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var w9 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var w10 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var enemies01 = [w1, w2, w3, w4, w5, w6, w7, w8, w9, w10];
-			level.enemies = enemies01;
-			level.counts = [2, 2, 3, 3, 4, 4, 5, 5, 6, 8];
-			level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 25];
-		}
+        // Set composition of each wave for Level 02
+        else if (game.data.level == 1) {
+            level.enemies = [c2CR, c2CA, c2CA, c3CCRA, c3CCRA, c2RRA, c2RRA, c3CRA, c3CRRA, c3CRRA];
+            level.counts = [3, 3, 3, 3, 4, 4, 4, 5, 5, 5];
+            level.timeGaps = [3, 5, 7, 9, 11, 2, 2, 2, 2, 2];
+        }
 
-		// Set composition of each wave for Level 02
-		else if (game.data.level == 1) {
-			w1 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w2 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w3 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w4 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w5 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w6 = ["robedSkeleton", "armoredSkeleton"];
-			w7 = ["robedSkeleton", "armoredSkeleton"];
-			w8 = ["robedSkeleton", "armoredSkeleton"];
-			w9 = ["robedSkeleton", "armoredSkeleton"];
-			w10 = ["robedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var enemies02 = [w1, w2, w3, w4, w5, w6, w7, w8, w9, w10];
-			level.enemies = enemies02;
-			level.counts = [10, 12, 14, 16, 18, 20, 22, 24, 26, 30];
-			level.timeGaps = [3, 5, 7, 9, 11, 2, 2, 2, 2, 2];
-		}
+        // Set composition of each wave for Level 03
+        else if (game.data.level == 2) {
+            level.enemies = [c2CR, c3CRA, c2CA, c3CCRA, c3CCRA, c2RA, c2RA, c3CRAA, c3CRRA, c3CRRA];
+            level.counts = [3, 3, 3, 3, 4, 4, 5, 5, 6, 6];
+            level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 21];
+        }
 
-		// Set composition of each wave for Level 03
-		else if (game.data.level == 2) {
-			w1 = ["clothedSkeleton"];
-			w2 = ["clothedSkeleton", "clothedSkeleton", "robedSkeleton"];
-			w3 = ["clothedSkeleton", "robedSkeleton", "robedSkeleton"];
-			w4 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w5 = ["clothedSkeleton", "clothedSkeleton", "armoredSkeleton"];
-			w6 = ["robedSkeleton", "armoredSkeleton"];
-			w7 = ["robedSkeleton", "armoredSkeleton"];
-			w8 = ["clothedSkeleton", "robedSkeleton", "robedSkeleton"];
-			w9 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w10 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var enemies03 = [w1, w2, w3, w4, w5, w6, w7, w8, w9, w10];
-			level.enemies = enemies03;
-			level.counts = [2, 2, 3, 3, 4, 4, 5, 4, 5, 6];
-			level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 25];
-		}
-
-		// Set composition of each wave for Level 04
-		else {
-			w1 = ["clothedSkeleton"];
-			w2 = ["clothedSkeleton", "clothedSkeleton", "robedSkeleton"];
-			w3 = ["clothedSkeleton", "robedSkeleton", "robedSkeleton"];
-			w4 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w5 = ["clothedSkeleton", "clothedSkeleton", "armoredSkeleton"];
-			w6 = ["robedSkeleton", "armoredSkeleton"];
-			w7 = ["robedSkeleton", "armoredSkeleton"];
-			w8 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w9 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			w10 = ["clothedSkeleton", "robedSkeleton", "armoredSkeleton"];
-			var enemies04 = [w1, w2, w3, w4, w5, w6, w7, w8, w9, w10];
-			level.enemies = enemies04;
-			level.counts = [2, 2, 3, 3, 4, 4, 5, 5, 6, 8];
-			level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 25];
-		}
+        // Set composition of each wave for Level 04
+        else {
+            level.enemies = [c1C, c2CCR, c2CRR, c3CRA, c2CCA, c2RA, c2RA, c3CRA, c3CRA, c3CRA];
+            level.counts = [2, 2, 3, 3, 4, 4, 5, 5, 6, 8];
+            level.timeGaps = [3, 10, 12, 12, 15, 15, 18, 18, 21, 25];
+        }
 
         // Load first level with a black background covering the default melonJS background
         me.levelDirector.loadLevel(TILE_LEVELS[game.data.level]);
